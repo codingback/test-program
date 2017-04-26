b992dad082d169341fde6dc8d61703710699e367
I059588 <kun.wu@sap.com>
2017-04-26 22:17:57 +0800

    test "123123-" values

--- src/TestClass.java
+++ src/TestClass.java
@@ -2,9 +2,22 @@
 public class TestClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("test");
-		System.out.println("new Change");
+		String testString1 = "1.232-";
+		String[] results = testString1.split("-");
+		
+		if(results.length == 1 && testString1.contains("-")){
+			try{
+				System.out.println(-Double.parseDouble(results[0]));
+			}catch(NumberFormatException e){
+				System.out.println("Not a number");
+			}
+			
+		}else{
+			System.out.println("not this value");
+		}
+		
+		
 	}
 
 }
+
